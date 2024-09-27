50c48b0484ae33ce853cfc3001a7fe6983cf0a2c
anjanshrestha8 <anjanshrestha2023@gmail.com>
2024-09-27 22:14:50 +0545

    first commit

---
+++ src/Component/App.jsx
@@ -0,0 +1,85 @@
+import { useState, useRef } from "react";
+import "../assets/css/app.css";
+
+function App() {
+  const [code, setCode] = useState(["", "", "", "", "", ""]);
+  const [message, setMessage] = useState("");
+  const inputRefs = useRef([]);
+  const apiUrl = import.meta.env.VITE_API_URL;
+  const handleChange = (value, index) => {
+    const newCode = [...code];
+    newCode[index] = value;
+    setCode(newCode);
+    if (value && index < 5) {
+      inputRefs.current[index + 1].focus();
+    }
+  };
+
+  const handlePaste = (e) => {
+    const pastedData = e.clipboardData.getData("text").slice(0, 6).split("");
+    const newCode = [...code];
+    pastedData.forEach((char, i) => {
+      if (i < newCode.length) {
+        newCode[i] = char;
+      }
+    });
+    setCode(newCode);
+    const lastIndex = Math.min(pastedData.length - 1, newCode.length - 1);
+    inputRefs.current[lastIndex]?.focus();
+  };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const reqCode = code.join("");
+    const response = await fetch(apiUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ code: reqCode }),
+    });
+    if (response.ok) {
+      setMessage("Verification successful!");
+    } else {
+      setMessage("Verification failed. Please try again.");
+    }
+  };
+
+  return (
+    <>
+      <h1>Enter Verification Code</h1>
+      <div className="form-wrapper">
+        <form onSubmit={handleSubmit}>
+          <div onPaste={handlePaste}>
+            {code.map((item, index) => {
+              return (
+                <>
+                  <input
+                    key={index}
+                    type="text"
+                    value={item}
+                    maxLength="1"
+                    onChange={(event) =>
+                      handleChange(event.target.value, index)
+                    }
+                    ref={(el) => (inputRefs.current[index] = el)}
+                  />
+                </>
+              );
+            })}
+          </div>
+          <br />
+          <br />
+          <div className="button-wrapper">
+            {" "}
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      </div>
+      <p className="message">
+        Message: <span>{message}</span>
+      </p>
+    </>
+  );
+}
+
+export default App;
